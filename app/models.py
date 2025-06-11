1ba1af9cd7e22ea9c19fa249938a18392e9d50a5
Huy Le <legiahuy050104@gmail.com>
2025-06-11 18:05:39 -0400

    docs added to models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
+"""
+/**
+ * @file models.py
+ * @brief Contains Pydantic models for request/response validation
+ * @author Huy Le (huyisme-005)
+ */
+
+"""
+# Imports BaseModel from pydantic for data validation
+
 from pydantic import BaseModel
 
 class NLRequest(BaseModel):
