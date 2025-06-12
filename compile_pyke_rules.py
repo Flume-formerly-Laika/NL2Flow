2466556592da93d68c8952d53676920ccb4ff047
Huy Le <legiahuy050104@gmail.com>
2025-06-12 16:45:20 -0400

    compile_pyke_rules updated again

--- compile_pyke_rules.py
+++ compile_pyke_rules.py
@@ -7,9 +7,9 @@
 from pyke import krb_compiler
 import os
 
-krb_file = "app/rules/field_mapping.krb"
-generated_root_pkg = "app.rules"
-generated_root_dir = os.path.dirname(krb_file)
+krb_file = os.path.join("app", "rules", "field_mapping.krb")
+generated_pkg = "app.rules"
+generated_dir = os.path.join("app", "rules")
 
-krb_compiler.compile_krb(krb_file, generated_root_pkg, generated_root_dir, krb_file)
-print("Compilation successful!")
+krb_compiler.compile_krb(krb_file, generated_pkg, generated_dir, krb_file)
+print("Rules compiled successfully.")
