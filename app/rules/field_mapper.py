5992689a3b13934c6847c950f1bc1e4bc3e46500
Huy Le <legiahuy050104@gmail.com>
2025-06-11 20:35:14 -0400

    documentation added

--- app/rules/field_mapper.py
+++ app/rules/field_mapper.py
@@ -1,9 +1,34 @@
-from pyke import knowledge_engine
+"""
+/**
+ * @file field_mapper.py
+ * @brief Maps field names using PyKE knowledge engine rules
+ * @author Huy Le (huyisme-005)
+ */
 
+
+"""
+
+# Import PyKE.knowledge_engine for field mapping rules
+from pyke import knowledge_engine
+'''
+/**
+ * @var engine
+ * @brief PyKE knowledge engine instance for field mapping rules
+ * @type knowledge_engine.engine
+ */
+'''
 engine = knowledge_engine.engine(__file__)
 engine.activate('field_mapping')
 
 def map_fields(field_dict):
+    """
+    
+     @brief Maps field names using PyKE knowledge engine rules
+     @param field_dict Dictionary of field names and their source values
+     @return dict Dictionary with mapped field names and values
+     @throws None (falls back to source value on error)
+     
+    """
     mapped = {}
     for name, source in field_dict.items():
         try:
