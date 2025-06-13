406574f61a54de765817df319a6e349a555b0ade
Huy Le <legiahuy050104@gmail.com>
2025-06-12 21:57:26 -0400

    models.py updated

--- app/models.py
+++ app/models.py
@@ -9,6 +9,9 @@
 # Imports BaseModel from pydantic for data validation
 
 from pydantic import BaseModel
+
+from typing import Any, Dict
+
 class NLRequest(BaseModel):
     """
     Pydantic model for natural language request validation.
@@ -17,3 +20,15 @@ class NLRequest(BaseModel):
         user_input (str): The natural language input from the user
     """
     user_input: str
+
+class FlowResponse(BaseModel):
+    '''
+    For responses
+
+    Attributes:
+    trace_id: given id
+
+    flow: the flow
+    '''
+    trace_id: str
+    flow: Dict[str, Any]
